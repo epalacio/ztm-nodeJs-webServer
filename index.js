6e384df65e003d5350c0e543ca860388a433b418
Esteban Palacio <estipalacio@gmail.com>
2021-07-09 14:42:31 -0400

    more complexity on server with parameterized urls

--- index.js
+++ index.js
@@ -6,22 +6,36 @@ const PORT = 3000;
 //creating a fresh server object
 const server = http.createServer();
 
+const friends = [
+    {
+        id: 0,
+        name: 'Nikola Tesla',
+    },
+    {
+        id: 1,
+        name: 'Sir Isaac Newton',
+    },
+];
+
 server.on('request', (req, res) => {
-    if (req.url === '/friends') {
+    //split the url to find the /parameterized url
+    const items = req.url.split('/');
+    console.log(items);
+    if (items[1] === '/friends') {
         //provide a successfull response with a 200 message
-        // res.writeHead(200, {
-        //     'Content-Type': 'application/json'
-        // });
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
 
-        //Needs to be called on each request that comes into the server. It indicates that the response is now complete:
-        res.end(JSON.stringify({
-            id: 329384,
-            name: 'Sir Isaac Newton',
-        }));
-    } else if (req.url === '/messages') {
-        res.setHeader('Content-Type', 'text/html')
+        if (items.length === 3) {
+            const friendIndex = Number(items[2]);
+            res.end(JSON.stringify(friends[friendIndex]))
+        } else {
+            //Needs to be called on each request that comes into the server. It indicates that the response is now complete:
+            res.end(JSON.stringify(friends));
+        }
+        
+    } else if (items[1] === '/messages') {
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<body>');
         res.write('<ul>');
